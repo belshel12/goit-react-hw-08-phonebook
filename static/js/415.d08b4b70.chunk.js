b0a09180d171abcdadb89ccf58937792a0472375
belshel12 <110160780+belshel12@users.noreply.github.com>
2023-04-21 19:05:14 +0000

    Deploying to gh-pages from @ belshel12/goit-react-hw-08-phonebook@58e178f6b117cebd393f4979f67157f43bc2d960 🚀

---
+++ static/js/415.d08b4b70.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var o=t(184);function r(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"PHONEBOOK"})})}}}]);
+//# sourceMappingURL=415.d08b4b70.chunk.js.map
